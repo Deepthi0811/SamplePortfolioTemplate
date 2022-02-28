a21e09b87a3d5b64de3a76379f68023b12139fcd
Deepthi <mani.deepthi2001@gmail.com>
2022-02-28 18:45:38 +0530

    Portfolio-angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { OneComponent } from './one/one.component';
+import { TwoComponent } from './two/two.component';
+import { FooterComponent } from './footer/footer.component';
+import { ThreeComponent } from './three/three.component';
+import { FourComponent } from './four/four.component';
+import { FiveComponent } from './five/five.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OneComponent,
+    TwoComponent,
+    FooterComponent,
+    ThreeComponent,
+    FourComponent,
+    FiveComponent
   ],
   imports: [
     BrowserModule,
